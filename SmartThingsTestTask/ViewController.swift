3461008320eb8a79d7398146c1edb8498f1d12ba
Artem <artem-orlov92@bk.ru>
2023-07-07 18:41:55 +0300

    Initial Commit

---
+++ SmartThingsTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartThingsTestTask
+//
+//  Created by Артем Орлов on 07.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
